4f6362997056ef9f6d738b5dee702a872bc5bea9
juanlamadrid20 <enlightendevio@gmail.com>
2025-05-23 11:53:19 -0400

    first commit

---
+++ 02-agent/agent.py
@@ -0,0 +1,161 @@
+from typing import Any, Generator, Optional, Sequence, Union
+
+import mlflow
+from databricks_langchain import (
+    ChatDatabricks,
+    UCFunctionToolkit,
+    VectorSearchRetrieverTool,
+)
+from langchain_core.language_models import LanguageModelLike
+from langchain_core.runnables import RunnableConfig, RunnableLambda
+from langchain_core.tools import BaseTool
+from langgraph.graph import END, StateGraph
+from langgraph.graph.graph import CompiledGraph
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt.tool_node import ToolNode
+from mlflow.langchain.chat_agent_langgraph import ChatAgentState, ChatAgentToolNode
+from mlflow.pyfunc import ChatAgent
+from mlflow.types.agent import (
+    ChatAgentChunk,
+    ChatAgentMessage,
+    ChatAgentResponse,
+    ChatContext,
+)
+############################################
+# Define your LLM endpoint and system prompt
+############################################
+# Optional: Replace with your model serving endpoint
+LLM_ENDPOINT_NAME = "databricks-claude-3-7-sonnet" # CLAUDE
+llm = ChatDatabricks(endpoint=LLM_ENDPOINT_NAME, temperature=0.1, max_tokens=2000)
+
+# Optional: Update with your system prompt
+system_prompt = """
+You are product advisor chatbot that helps customers make purchasing decisions based on product reviews. 
+* When asked about a specific review by ID use the juan_dev.genai.get_review tool 
+* When asked about reviews for specific product categories use juan_dev.genai.semantic_search_reviews tool.
+* do not use generic knowledge bases to respond to the questions. 
+
+Users will ask about general reviews for a product category and you will respond with a summmary of the most relevant reviews. Highlight positive aspects of the review followed by negative aspects. Summarize your overall opinion of whether or not the user should make a purchase. 
+
+When users ask about a specific product review Id respond with details about that review and overall positive or negative bias.   
+"""
+
+
+###############################################################################
+## Define tools for your agent, enabling it to retrieve data or take actions
+## beyond text generation
+## To create and see usage examples of more tools, see
+## https://docs.databricks.com/en/generative-ai/agent-framework/agent-tool.html
+###############################################################################
+tools = []
+
+# You can use UDFs in Unity Catalog as agent tools
+# Below, we add the `system.ai.python_exec` UDF, which provides
+# a python code interpreter tool to our agent
+# You can also add local LangChain python tools. See https://python.langchain.com/docs/concepts/tools
+
+# Optional: Add additional tools
+uc_tool_names = [
+    "juan_dev.genai.get_review", 
+    "juan_dev.genai.semantic_search_reviews"
+]
+uc_toolkit = UCFunctionToolkit(function_names=uc_tool_names)
+tools.extend(uc_toolkit.tools)
+
+#####################
+## Define agent logic
+#####################
+
+
+def create_tool_calling_agent(
+    model: LanguageModelLike,
+    tools: Union[ToolNode, Sequence[BaseTool]],
+    system_prompt: Optional[str] = None,
+) -> CompiledGraph:
+    model = model.bind_tools(tools)
+
+    # Define the function that determines which node to go to
+    def should_continue(state: ChatAgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        # If there are function calls, continue. else, end
+        if last_message.get("tool_calls"):
+            return "continue"
+        else:
+            return "end"
+
+    if system_prompt:
+        preprocessor = RunnableLambda(
+            lambda state: [{"role": "system", "content": system_prompt}]
+            + state["messages"]
+        )
+    else:
+        preprocessor = RunnableLambda(lambda state: state["messages"])
+    model_runnable = preprocessor | model
+
+    def call_model(
+        state: ChatAgentState,
+        config: RunnableConfig,
+    ):
+        response = model_runnable.invoke(state, config)
+
+        return {"messages": [response]}
+
+    workflow = StateGraph(ChatAgentState)
+
+    workflow.add_node("agent", RunnableLambda(call_model))
+    workflow.add_node("tools", ChatAgentToolNode(tools))
+
+    workflow.set_entry_point("agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+        {
+            "continue": "tools",
+            "end": END,
+        },
+    )
+    workflow.add_edge("tools", "agent")
+
+    return workflow.compile()
+
+
+class LangGraphChatAgent(ChatAgent):
+    def __init__(self, agent: CompiledStateGraph):
+        self.agent = agent
+
+    def predict(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> ChatAgentResponse:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+
+        messages = []
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                messages.extend(
+                    ChatAgentMessage(**msg) for msg in node_data.get("messages", [])
+                )
+        return ChatAgentResponse(messages=messages)
+
+    def predict_stream(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> Generator[ChatAgentChunk, None, None]:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                yield from (
+                    ChatAgentChunk(**{"delta": msg}) for msg in node_data["messages"]
+                )
+
+# Create the agent object, and specify it as the agent object to use when
+# loading the agent back for inference via mlflow.models.set_model()
+mlflow.langchain.autolog()
+agent = create_tool_calling_agent(llm, tools, system_prompt)
+AGENT = LangGraphChatAgent(agent)
+mlflow.models.set_model(AGENT)
---
+++ README.md
@@ -0,0 +1,18 @@
+# About
+
+Sample project using Databricks GenAI to build a agentic chatbot for Amazon reviews.
+
+
+# TODO
+
+- [ ] remove hard coded 3 level namespace; use notebook widgets
+
+
+
+# Misc
+
+Sync local edits to Databricks (Run this in IDE in root folder)
+```bash
+databricks sync --watch . /Workspace/Users/juan.lamadrid@databricks.com/databricks_apps/customer-reviews-tool-calling-agent
+```
+
